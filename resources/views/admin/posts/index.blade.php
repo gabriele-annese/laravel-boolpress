b5e2c9b13361238e5a9498f9ce28781555e8a49f
gabriele-annese <gannese06@gmail.com>
2022-01-31 20:16:38 +0100

    add index posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -12,7 +12,26 @@
             </p>
         
         @else
-            
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th colspan="3">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post )
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->title}}</td>
+                            <td>SHOW</td>
+                            <td>EDIT</td>
+                            <td>DELETE</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         @endif
     </div>
 @endsection
