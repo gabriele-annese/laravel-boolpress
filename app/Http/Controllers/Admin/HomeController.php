563af9aac53cfc1eb1f13d3d12d928943b8a9e5d
gabriele-annese <gannese06@gmail.com>
2022-02-11 17:30:20 +0100

    add locale it

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index(){
+
+        //carbon
+        $dt = Carbon::now();
+        dump($dt->isoFormat('dddd DD/MM/YYYY'));
+        dump( $dt->locale() );
+        
+
         return view('admin.home');
     }
 }
