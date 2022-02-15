180939ec7553f57485b53b16bf872cff50f45dd2
gabriele-annese <gannese06@gmail.com>
2022-02-15 14:23:26 +0100

    add user

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,14 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\SendWelcomeEmail;
+
 class HomeController extends Controller
 {
     public function index(){
 
+        //email
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail(Auth::user()->name));
+
         //carbon
         $dt = Carbon::now();
-        dump($dt->isoFormat('dddd DD/MM/YYYY'));
-        dump( $dt->locale() );
+        // dump($dt->isoFormat('dddd DD/MM/YYYY'));
+        // dump( $dt->locale() );
         
 
         return view('admin.home');
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,4 @@
+
+<h1>Welcome, {{ $name }}!</h1>
+
+<p>hello there, welcome to our site...</p>
